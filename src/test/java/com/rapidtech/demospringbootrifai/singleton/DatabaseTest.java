4932842e2ab8b04cdf4b0b5603869c12f1402a64
mhrifai <mkhrifai99@gmail.com>
2022-12-19 10:06:06 +0700

    singleton

---
+++ src/test/java/com/rapidtech/demospringbootrifai/singleton/DatabaseTest.java
@@ -0,0 +1,17 @@
+package com.rapidtech.demospringbootrifai.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+
+    @Test
+    void singletonTest() {
+        Database database1 = Database.getInstance();
+        Database database2 = Database.getInstance();
+        Database database3 = Database.getInstance();
+
+        Assertions.assertSame(database1, database2);
+        Assertions.assertSame(database1, database3);
+    }
+}
