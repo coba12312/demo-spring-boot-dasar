4ba11221f23fe10033c730e1995057fc600b4580
mhrifai <mkhrifai99@gmail.com>
2022-12-19 11:12:19 +0700

    primary bean

--- src/main/java/com/rapidtech/demospringbootrifai/config/DuplicateBeanConfiguration.java
+++ src/main/java/com/rapidtech/demospringbootrifai/config/DuplicateBeanConfiguration.java
@@ -3,18 +3,21 @@ package com.rapidtech.demospringbootrifai.config;
 import com.rapidtech.demospringbootrifai.model.Foo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DuplicateBeanConfiguration {
-    @Bean
+    @Primary
+    @Bean(value = "foo1")
     public Foo foo1(){
         return new Foo();
     }
 
-    @Bean
+    @Bean(value = "foo2")
     public Foo foo2(){
         return new Foo();
     }
 }
+
 
 
--- src/test/java/com/rapidtech/demospringbootrifai/config/DuplicateBeanConfigurationTest.java
+++ src/test/java/com/rapidtech/demospringbootrifai/config/DuplicateBeanConfigurationTest.java
@@ -2,17 +2,20 @@ package com.rapidtech.demospringbootrifai.config;
 
 import com.rapidtech.demospringbootrifai.model.Foo;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DuplicateBeanConfigurationTest {
+    private static ApplicationContext context;
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(DuplicateBeanConfiguration.class);
+    }
+
     @Test
     void duplicateBeanTest() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DuplicateBeanConfiguration.class);
-
         Foo foo1 = context.getBean("foo1", Foo.class);
         Foo foo2 = context.getBean("foo2", Foo.class);
 
@@ -24,4 +27,16 @@ class DuplicateBeanConfigurationTest {
         Foo foo4 = context.getBean("foo2", Foo.class);
         Assertions.assertSame(foo2, foo4);
     }
+
+    @Test
+    void primaryBeanTest() {
+        Foo foo1 = context.getBean(Foo.class);
+        Foo foo2 = context.getBean("foo1", Foo.class);
+
+        Assertions.assertSame(foo1, foo2);
+
+        Foo foo3 = context.getBean("foo2", Foo.class);
+        Assertions.assertNotSame(foo1, foo3);
+    }
 }
+
